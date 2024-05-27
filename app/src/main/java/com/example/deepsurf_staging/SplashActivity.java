b5bbcb3b8f6c5d3b123e1e1e31a72321fe4f29ec
Jakaria-Jihad <jakariahoseen669@gmail.com>
2024-05-27 15:04:52 +0600

    Added Splash Activity and updated for launcher in AndroidManifest.xml

--- app/src/main/java/com/example/deepsurf_staging/SplashActivity.java
+++ app/src/main/java/com/example/deepsurf_staging/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.example.deepsurf_staging;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,13 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 1000);
     }
 }
